68354c4f6d5f1aab3c08925cc2438a9e8407da06
Liberbell <liberbell@gmail.com>
2020-12-30 14:34:43 +0900

    views2

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     # return HttpResponse("Welcome to Django page.")
-    return render(request, 'blog/home.html')
+    return render(request, 'blog/index.html')
