b39d5ce76e6579468538ac6b106048455eb4d48b
Liberbell <liberbell@gmail.com>
2020-12-31 12:54:23 +0900

    urls2

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="index")
-    path('business', views.business, name='business')
+    path('', views.index, name="index"),
+    path('business', views.business, name='business'),
 ]
