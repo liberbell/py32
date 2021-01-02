0212d296ea00ab510b9596cdaf7dff573b74e3b9
Liberbell <liberbell@gmail.com>
2021-01-02 13:52:40 +0900

    models3

--- django_project/blog/models.py
+++ django_project/blog/models.py
@@ -5,3 +5,5 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.models.DateTimeField(default=timezone.now)
