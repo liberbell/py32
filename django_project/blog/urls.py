893a6fc34d6493de43b0e0f1104163454719823a
Liberbell <liberbell@gmail.com>
2021-01-04 09:17:16 +0900

    urls2

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -5,4 +5,5 @@ from users import views as user_view
 urlpatterns = [
     path('', views.index, name="index"),
     path('business', views.business, name='business'),
+    path('register/', user_view.register, name='register')
 ]
