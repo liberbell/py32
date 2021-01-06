1b5a562edc47adf04815b0b222b1eeb0d1b9e7ec
Liberbell <liberbell@gmail.com>
2021-01-07 08:57:44 +0900

    views3

--- django_project/users/views.py
+++ django_project/users/views.py
@@ -6,7 +6,7 @@ from .forms import UserRegisterForm
 # Create your views here.
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
@@ -14,6 +14,6 @@ def register(request):
             return redirect('index')
 
     else:
-        form = UserCreationForm()
+        form = UserRegisterForm()
 
     return render(request, 'users/register.html', {'form': form})
