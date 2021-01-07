cd2171d8df60481878bf87ed12bb4253f0f1b85a
Liberbell <liberbell@gmail.com>
2021-01-08 08:41:24 +0900

    urls1

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
 from users import views as user_view
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.index, name="index"),
     path('business', views.business, name='business'),
     path('register/', user_view.register, name='register'),
+    path('')
 ]
