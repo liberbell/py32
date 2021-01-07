0b548f8cdb0c1d90a66819686e1463792cd5d714
Liberbell <liberbell@gmail.com>
2021-01-08 08:56:17 +0900

    urls2

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('', views.index, name="index"),
     path('business', views.business, name='business'),
     path('register/', user_view.register, name='register'),
-    path('login', auth_views.LoginView.as_view(), name='login'),
+    path('login', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout', auth_views.LogoutView.as_view(), name='logout'),
 ]
