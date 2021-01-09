4174bf795c805a6ce2c38046d0c5a1ee5ae950db
Liberbell <liberbell@gmail.com>
2021-01-09 17:10:07 +0900

    views1

--- django_project/users/views.py
+++ django_project/users/views.py
@@ -11,7 +11,7 @@ def register(request):
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, 'Account created!')
-            return redirect('index')
+            return redirect('login')
 
     else:
         form = UserRegisterForm()
