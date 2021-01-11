ef17df1f1ec5c5ca15db0bda5e7c5c30f3f2e31c
Liberbell <liberbell@gmail.com>
2021-01-12 08:51:04 +0900

    urls2

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -13,3 +13,9 @@ urlpatterns = [
     path('logout', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('profile', user_view.profile, name='profile'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += [
+        static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    ]
+    
