2c8e33e42c0eabfc4aa1cb07ff589fb1a0e2e20e
Liberbell <liberbell@gmail.com>
2021-01-13 08:47:58 +0900

    forms1

--- django_project/users/forms.py
+++ django_project/users/forms.py
@@ -8,3 +8,7 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+class UserUpdateForm(forms.ModelForm):
+    class Meta:
+        model = User
