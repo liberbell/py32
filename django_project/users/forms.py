23d668db5d6cc3783ea08dfc45220be26be1d8d4
Liberbell <liberbell@gmail.com>
2021-01-13 08:50:19 +0900

    forms2

--- django_project/users/forms.py
+++ django_project/users/forms.py
@@ -13,3 +13,7 @@ class UserUpdateForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['username']
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
