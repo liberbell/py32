ac855b209b5900bb3322ba153b7ffd7b5e9b26fb
Liberbell <liberbell@gmail.com>
2021-01-14 09:18:34 +0900

    views2

--- django_project/users/views.py
+++ django_project/users/views.py
@@ -4,6 +4,7 @@ from django.contrib import messages
 from .forms import UserRegisterForm
 from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
+from django.views.decorators.csrf import csrf_protect, csrf_exempt
 
 # Create your views here.
 def register(request):
