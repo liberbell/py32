2c8e87c1bf30418c24f5e4313d043f11a3cda554
Liberbell <liberbell@gmail.com>
2021-01-14 09:31:38 +0900

    views2

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
+from django.views.generic import ListView
+
 
 # Create your views here.
 def index(request):
