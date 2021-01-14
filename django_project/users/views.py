b4a81f5174402f4f82e5f214a71d95bd777817c5
Liberbell <liberbell@gmail.com>
2021-01-14 09:02:31 +0900

    views4

--- django_project/users/views.py
+++ django_project/users/views.py
@@ -26,6 +26,10 @@ def profile(request):
         u_form = UserUpdateForm(request.POST, instance=request.user)
         p_form = ProfileUpdateForm(request.POST, request.FILE, instance=request.user.profile)
 
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+
     else:
         u_form = UserUpdateForm(instance=request.user)
         p_form = ProfileUpdateForm(instance=request.user.profile)
