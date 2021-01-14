a03f20948cbca6e93469d9a48ca8da0818bf079b
Liberbell <liberbell@gmail.com>
2021-01-14 09:30:33 +0900

    views1

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -7,21 +7,6 @@ def index(request):
     # return HttpResponse("Welcome to Django page.")
     return render(request, 'blog/index.html')
 
-posts =[
-    {'author' : 'django1',
-    'title': 'Blog Post1',
-    'content' : 'First post content',
-    'date_posted' : 'December 31, 2020'
-    },
-    {'author' : 'test user1',
-    'title': 'Blog Post2',
-    'content' : 'Second post content',
-    'date_posted' : 'January 1, 2021'
-    }
-]
-# def business(request):
-#     return render(request, 'blog/business.html')
-
 def business(request):
     context = {
         'posts' : Post.objects.all()
