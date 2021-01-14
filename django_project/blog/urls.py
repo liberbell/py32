b6f9a1255972cfa035557eb3e939960e0e2190d0
Liberbell <liberbell@gmail.com>
2021-01-14 09:36:23 +0900

    urls1

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -4,10 +4,11 @@ from users import views as user_view
 from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import PostListView
 
 urlpatterns = [
     path('', views.index, name="index"),
-    path('business', views.business, name='business'),
+    path('business', PostListView.as_view(), name='business'),
     path('register/', user_view.register, name='register'),
     path('login', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
