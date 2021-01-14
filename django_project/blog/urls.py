aad290ba67214d9d0db27b46af3268ded49e9a75
Liberbell <liberbell@gmail.com>
2021-01-15 08:43:12 +0900

    urls1

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -4,7 +4,7 @@ from users import views as user_view
 from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import PostListView
+from .views import PostListView, PostDetailView
 
 urlpatterns = [
     path('', views.index, name="index"),
@@ -13,6 +13,7 @@ urlpatterns = [
     path('login', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('profile', user_view.profile, name='profile'),
+    path('post/<int:pk>', PostDetailView.as_view(), name='post-detail'),
 ]
 
 if settings.DEBUG:
