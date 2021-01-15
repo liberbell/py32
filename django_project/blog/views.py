4dd5c11ea48c8c27b4e6d3639f9d1124d3fbf33a
Liberbell <liberbell@gmail.com>
2021-01-15 09:03:49 +0900

    views3

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 
 
 # Create your views here.
@@ -22,4 +22,7 @@ class PostListView(ListView):
     ordering = ['-date_posted']
 
 class PostDetailView(DetailView):
+    model = Post
+
+class PostCreateView(CreateView):
     model = Post
