45c96386d82d3a2f47279a66e809cdd8aaf2ba4c
Liberbell <liberbell@gmail.com>
2021-01-17 14:19:48 +0900

    views4

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -4,7 +4,6 @@ from .models import Post
 from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from django.contrib.auth.mixins import UserPassesTestMixin
 
-
 # Create your views here.
 def index(request):
     # return HttpResponse("Welcome to Django page.")
@@ -33,10 +32,17 @@ class PostCreateView(CreateView):
         form.instance.author = self.request.user
         return super().form_valid(form)
 
-class PostUpdateView(UpdateView):
+class PostUpdateView(UserPassesTestMixin, UpdateView):
     model = Post
     fields = ['title', 'content']
 
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        else:
+            return False
