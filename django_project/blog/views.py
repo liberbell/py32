ca39ff395fa20e6303c5db1100ec84a6eeefad64
Liberbell <liberbell@gmail.com>
2021-01-17 14:15:35 +0900

    views2

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
 from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.contrib.auth.mixins import UserPassesTestMixin
 
 
 # Create your views here.
