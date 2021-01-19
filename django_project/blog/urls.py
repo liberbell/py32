8c4320f29bf8991ee8b64d9714952153ca0fe2c8
Liberbell <liberbell@gmail.com>
2021-01-20 08:43:03 +0900

    urls1

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from users import views as user_view
+from users import views as user_views
 from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
@@ -11,10 +11,10 @@ from blog.models import Post
 urlpatterns = [
     path('', views.index, name="index"),
     path('business', PostListView.as_view(), name='business'),
-    path('register/', user_view.register, name='register'),
+    path('register/', user_views.register, name='register'),
     path('login', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
-    path('profile', user_view.profile, name='profile'),
+    path('profile', user_views.profile, name='profile'),
     path('post/<int:pk>', PostDetailView.as_view(), name='post-detail'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/update', PostUpdateView.as_view(), name='post-update'),
