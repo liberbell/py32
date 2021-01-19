479d817cee46495c68ac2920ee339c3c5e27cb9e
Liberbell <liberbell@gmail.com>
2021-01-20 08:47:39 +0900

    views2

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from .models import Post
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import UserPassesTestMixin
+from django.views.generic.dates import YearArchiveView
 
 # Create your views here.
 def index(request):
